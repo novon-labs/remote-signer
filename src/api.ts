9860b732c6012aa1c039935c3c99478b8c082aa0
ammarlakho <ammar1lakho@gmail.com>
2021-12-15 17:11:21 +0500

    change signature data type to hex

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import {SecretKey, sign} from "@chainsafe/bls";
+import {init, PublicKey, SecretKey, sign} from "@chainsafe/bls";
 import {hexToBytes, bytesToHex} from './utils'
 import {publicToPrivate} from "./index"
 
@@ -13,15 +13,21 @@ export const getKeys = async(): Promise<Array<Map<string, string>>> => {
     
 export const signData = async(identifier: string, data: string): Promise<any> => {
     // let {bls_domain, data, fork, genesis_validators_root} = req.body;
+    await init("blst-native");
+    const pk = PublicKey.fromHex(identifier);
+    console.log("pl", pk)
+    console.log("pl", pk.toBytes())
+    
 
     if(!publicToPrivate.has(identifier) && !publicToPrivate.has(identifier.slice(2)) ) {
         return {error: `Key not found: ${identifier}`}
     }
     const dataBytes = Uint8Array.from(hexToBytes(data));    
-
+    
     const skHex = publicToPrivate.get(identifier) || publicToPrivate.get(identifier.slice(2));
     const secretKey = SecretKey.fromHex(skHex);
     const sig = secretKey.sign(dataBytes);
-    return {"signature": sig.toBytes()}
+    console.log(sig.toBytes())
+    return {"signature": sig.toHex()}
 }
 
--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,29 @@ const publicToPrivate = new Map();
 
 (async () => {
     await init("herumi");
-  
-    for(let i = 0; i < 1; i++) {
-        const secretKey = SecretKey.fromHex("0x68081afeb7ad3e8d469f87010804c3e8d53ef77d393059a55132637206cc59ec");
+    const secretKeys = 
+        [
+            "0x68081afeb7ad3e8d469f87010804c3e8d53ef77d393059a55132637206cc59ec",
+            "0x25295f0d1d592a90b333e26e85149708208e9f8e8bc18f6c77bd62f8ad7a6866", 
+            "0x51d0b65185db6989ab0b560d6deed19c7ead0e24b9b6372cbecb1f26bdfad000", 
+            "0x315ed405fafe339603932eebe8dbfd650ce5dafa561f6928664c75db85f97857",
+            "0x25b1166a43c109cb330af8945d364722757c65ed2bfed5444b5a2f057f82d391",
+            "0x3f5615898238c4c4f906b507ee917e9ea1bb69b93f1dbd11a34d229c3b06784b",
+            "0x055794614bc85ed5436c1f5cab586aab6ca84835788621091f4f3b813761e7a8",
+            "0x1023c68852075965e0f7352dee3f76a84a83e7582c181c10179936c6d6348893",
+            "0x3a941600dc41e5d20e818473b817a28507c23cdfdb4b659c15461ee5c71e41f5",
+        ]
+        console.log(secretKeys.length);
+    for(let i = 0; i < secretKeys.length; i++) {
+        const secretKey = SecretKey.fromHex(secretKeys[i]);
         // const message = Uint8Array.from(hexToBytes("b6bb8f3765f93f4f1e7c7348479289c9261399a3c6906685e320071a1a13955c"))
         const sk = secretKey.toBytes();
         const pk = secretKeyToPublicKey(sk);
         
         publicToPrivate.set(bytesToHex(pk).slice(2), bytesToHex(sk).slice(2));
+        // publicToPrivate.set(bytesToHex(pk2).slice(2), bytesToHex(sk2).slice(2));
     }
-    // console.log(publicToPrivate);
+    console.log(publicToPrivate);
 
 })();
 
